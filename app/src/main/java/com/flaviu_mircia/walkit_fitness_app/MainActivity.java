c7679dc6a4ee42c0d969465167a102463f7e7555
flaviumircia <flaviumircia15@gmail.com>
2022-03-24 19:04:01 +0200

    Uploaded the Walk it application alpha build 0.1

---
+++ app/src/main/java/com/flaviu_mircia/walkit_fitness_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.flaviu_mircia.walkit_fitness_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+    }
+}
