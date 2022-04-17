c01a98b0da9b44d6b18949b1b934e6efede360df
flaviumircia <flaviumircia15@gmail.com>
2022-04-17 12:42:10 +0300

    Few changes for cleaner-code

--- app/src/main/java/com/flaviu_mircia/walkit_fitness_app/forgotPassword.java
+++ app/src/main/java/com/flaviu_mircia/walkit_fitness_app/forgotPassword.java
@@ -1,11 +1,14 @@
 package com.flaviu_mircia.walkit_fitness_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -13,46 +16,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class forgotPassword extends AppCompatActivity {
-    private EditText emailTextLogin;
-    private TextView emailTextView;
-    private ImageView backgroundImageView;
+    private EditText emailText;
     private TextView signUpTextView;
-    private TextView reset;
+    private TextView resetButton;
+    private TextView infotext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notifications bar
         setContentView(R.layout.activity_forgot_password);
 
-        emailTextLogin =(EditText) findViewById(R.id.EmailReg);
-        emailTextView =(TextView)  findViewById(R.id.EmailTextView);//links emailTextView and EmailTextLogin to specific layout
-
+        emailText =(EditText) findViewById(R.id.Email);
         signUpTextView=(TextView) findViewById(R.id.signUpTextView);
-
-        backgroundImageView=(ImageView) findViewById(R.id.imageView);
-
-        reset=(TextView)findViewById(R.id.restPasswordTextView);
-
-        emailTextLogin.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                emailTextView.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        }); //adds change to email text view on change to emailTextLogin
-
-        backgroundImageView.setOnClickListener(new View.OnClickListener() {
+        resetButton=(TextView) findViewById(R.id.resetPasswordTextView);
+        infotext=(TextView) findViewById(R.id.infoTextPass);
+        resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(emailTextLogin.length()==0){
-                    emailTextView.setVisibility(View.VISIBLE);
-                }
+                infotext.setGravity(Gravity.CENTER);
+                infotext.setText("We sent you an e-mail for resetting your password.");
+                emailText.setVisibility(View.GONE);
+                resetButton.setVisibility(View.GONE);
             }
         });
 
@@ -62,13 +46,6 @@ public class forgotPassword extends AppCompatActivity {
                 startActivity(new Intent(forgotPassword.this,register_activity.class));
             }
         });
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$ regex email adress
-                if(emailTextLogin.length()!=0)
-                startActivity(new Intent(forgotPassword.this,passwordReseted.class));
-            }
-        });
     }
+
 }
--- app/src/main/java/com/flaviu_mircia/walkit_fitness_app/register_activity.java
+++ app/src/main/java/com/flaviu_mircia/walkit_fitness_app/register_activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,49 +19,28 @@ import android.widget.TextView;
 import java.util.Objects;
 
 public class register_activity extends AppCompatActivity {
-    private TextView passwordTextView;
-    private TextView passwordConfirmTextView;
-    private TextView userNameTextView;
-    private TextView emailAdressTextView;
     private EditText emailEditText;
     private EditText userNameEditText;
     private EditText passwordEditText;
     private EditText passwordConfirmEditText;
-    private ImageView backgroundImage;
-
+    private TextView signupbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notifications bar
         setContentView(R.layout.activity_register);
-        backgroundImage=(ImageView) findViewById(R.id.imageView);
         passwordEditText=(EditText) findViewById(R.id.PasswordReg);
-        passwordTextView=(TextView) findViewById(R.id.passwordTextView);
+        passwordConfirmEditText=(EditText) findViewById(R.id.passwordConfirm);
 
-        passwordEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        signupbutton=(TextView) findViewById(R.id.restPasswordTextView);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                        passwordTextView.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
-        backgroundImage.setOnClickListener(new View.OnClickListener() {
+        signupbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(passwordEditText.length()==0){
-                    passwordTextView.setVisibility(View.VISIBLE);
-                }
+//                if(passwordEditText.toString().equals(passwordConfirmEditText) && passwordEditText.toString().length()>=3){
+//                }
             }
         });
+
     }
 }
